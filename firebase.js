87c257a617838e80f3db50a5bbb5c4291e61e1bd
weversonveg <weversonveg@gmail.com>
2026-09-15 09:48:52 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,10 @@
+// Importa as funções do Firebase direto da nuvem do Google
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore, enableIndexedDbPersistence } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
+// Sua configuração oficial do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBM4meI3r8hojjpI3n0wJsORV5h3KFH9M",
+  apiKey: "AIzaSyBM4YmeI3r8hojjpI3n0wJsOrV5h3KFH9M",
   authDomain: "portal-47793.firebaseapp.com",
   projectId: "portal-47793",
   storageBucket: "portal-47793.firebasestorage.app",
@@ -11,11 +12,11 @@ const firebaseConfig = {
   appId: "1:746031374293:web:ddb18d9faec8530a72675c"
 };
 
+// Inicializa o Firebase e o Firestore
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
-export const auth = getAuth(app);
 
+// Ativa a persistência offline via cache local (IndexedDB)
 enableIndexedDbPersistence(db)
   .catch((err) => {
     if (err.code === 'failed-precondition') {
